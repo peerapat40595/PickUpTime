c8ca281d3b9d8c13c5a82abd24c7d88b272fccdf
selseo <silversoul.sevendeadlysins@gmail.com>
2016-04-17 01:49:30 +0700

    create event from without backend

--- nscript.js
+++ nscript.js
@@ -5,25 +5,43 @@ $("#createevent").click(function(e){
     e.preventDefault();
     var name = $('<input>',{type: "text",name: "name", value: "Enter event name"});
     var desc = $('<input>',{type: "text",name: "description", value: "Type Description"});
-    var addtime = $('<button>',{id: "addslot",text: "Add time slot"});
+    var addtime = $('<button>',{id: "addslot",type: "button" ,text: "Add time slot"});
     var timelist = $('<div>',{id: "timeslotlist"});
-    $('<form>').addClass("cevent").append(name).append(desc).append(addtime).append(timelist).appendTo("div#content");
-    var originalState2=$("div#timeslotlist").html();
-});
-$("#addslot").click(function(e){
+    var timelistadd = $('<div>',{id: "timeslotlistadd"});
+    $('<div>').addClass("cevent").append(name).append(desc).append(addtime).append(timelist).append(timelistadd).appendTo("div#content");
+
+    var originalState2 = $("div#timeslotlistadd").html();
+    $("#addslot").click(function(e){
     //var originalState = $("div#content").html();
     //$("div#content").html(originalState);
     //$("div#content").html(originalState);
-    $("div#timeslotlist").html(originalState2);
-    e.preventDefault();
+    $("div#timeslotlistadd").html(originalState2);
+      e.preventDefault();
+      var date = $('<input>',{type: "text",name: "date", value: "Enter date"});
+      var from = $('<input>',{type: "text",name: "from", value: "Enter time begin"});
+      var to = $('<input>',{type: "text",name: "to", value: "Enter time finish"});
+      var adds = $('<button>',{text: "Add",type: "button",class:"add"});
+      $('<div>').addClass("timeslota").append(date).append(from).append(to).append(adds).appendTo("div#timeslotlistadd");
+      $(".add").click(function(e){
+        var sdate=$('input[name=date]').val();
+        var sfrom=$('input[name=from]').val();
+        var sto=$('input[name=to]').val();
+        var del=$('<span>',{text:"click to delete"});
+        $(".timeslota").remove();
+        originalState2 = $("div#timeslotlistadd").html();
+        $('<div>').addClass("timeslot").append(sdate+" "+sfrom+" "+sto+" ").append(del).appendTo("div#timeslotlist");
+        $(document).on('click', '.timeslot', function() {
+          $(this).remove();
+        });
+      });
+    });
+});
+/**/
+/*function test(){
     var date = $('<input>',{type: "text",name: "date", value: "Enter date"});
     var from = $('<input>',{type: "text",name: "description", value: "Enter time begin"});
     var to = $('<input>',{type: "text",name: "description", value: "Enter time finish"});
-    var adds = $('<button>',{text: "Add"});
-    //var toAdd = $('input[name=checkListItem]').val();
-   // $(".list").append('<div class="item">' + toAdd + '</div>'); 
-    //$("#timeslotlist").append('<form class="timeslot">' + date+from+to+adds + '</form>'); 
-    $("#timeslotlist").append('<div class="timeslot"> test </div>'); 
-    //$('<form>').addClass("timeslot").append(date).append(from).append(to).append(adds).appendTo("div#timelist");
-});
+    var adds = $('<button>',{text: "Add",type: "button"});
+    $('<div>').addClass("timeslot").append(date).append(from).append(to).append(adds).appendTo("div#timeslotlist");
+}*/
 
